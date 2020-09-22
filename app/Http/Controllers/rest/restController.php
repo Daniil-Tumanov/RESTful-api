a38170b2427c5eb3826d5d017a816b7aa3783106
Daniil-Tumanov <Daniil.CSKA9@gmail.com>
2020-09-22 22:46:02 +0300

    Added image upload

--- app/Http/Controllers/rest/restController.php
+++ app/Http/Controllers/rest/restController.php
@@ -16,55 +16,57 @@ class restController extends Controller
     public function dishById($id){
         $dishes = RestModel::find($id);
         if(is_null($dishes)){
-            return response()->json(['status code' => 404, 'status text' => 'Dish not found', 'body'=>'message: Dish not found'], 404);
+            return response()->json(['status code' => 404, 'status text' => 'Dish not found', 'message' => 'Dish not found'], 404);
         }
-        return response()->json(['status code'=>200, 'status text:'=>'View dish', 'Dish'=>RestModel::find($id)], 200);
+        return response()->json(['status code'=> 200, 'status text'=>'View dish', 'Dish'=>RestModel::find($id)], 200);
     }
 
     public function addDish(Request $req){
         $rules = [
-            'title' => 'required|min:2',
+            'title' => 'required|unique:dishes|min:2',
             'description' => 'required|min:2',
             'composition' => 'required|min:2',
-            'cost' => 'required|min:2',
-            'tags' => 'required|min:2',
-            'img' => 'required|min:2'
+            'cost' => 'required|integer',
+            'img' => 'required|image|mimes:jpg,jpeg,png|max:2048'
         ];
         $validator = Validator::make($req->all(), $rules);
         if($validator->fails()){
             return response()->json($validator->errors(), 400);
         }
-        $dishes = RestModel::create($req->all());
-        return response()->json($dishes, 201);
+        if ($req->hasFile('img')) {
+            $image = $req->file('img');
+            $name = $image->getClientOriginalName();                    
+            $destinationPath = public_path('/images');
+            $image->move($destinationPath, $name);
+
+            $dishes = new RestModel();
+            $dishes->title = $req->title;
+            $dishes->description = $req->description;
+            $dishes->composition = $req->composition;
+            $dishes->cost = $req->cost;
+            $dishes->tags = $req->tags;
+            $dishes->img = $name;
+
+            $dishes->save();
+        }   
+        return response()->json(['status code' => 201, 'status text' => 'Successful creation', 'status' => 'true', 'message'=> $dishes->id], 201);
     }
 
     public function editDish(Request $req, $id){
-       /* $rules = [
-            'title' => 'required|min:2',
-            'description' => 'required|min:2',
-            'composition' => 'required|min:2',
-            'cost' => 'required|min:2',
-            'tags' => 'required|min:2',
-            'img' => 'required|min:2'
-        ];
-        $validator = Validator::make($req->all(), $rules);
-        if($validator->fails()){
-            return response()->json($validator->errors(), 400);
-        }*/
         $dishes = RestModel::find($id);
         if(is_null($dishes)){
-            return response()->json(['status code' => 400, 'status text' => 'Editing error', 'body'=>'status: false, Dish not found'], 404);
+            return response()->json(['status code' => 400, 'status text' => 'Editing error', 'status' => 'false', 'message' => 'Dish not found'], 404);
         }
         $dishes->where('id', $id)->update($req->all());
-        return response()->json(['status code'=>201, 'status text:'=>'View dish', 'Dish'=>$dishes], 200);
+        return response()->json(['status code' => 201, 'status text:'=>'View dish', 'Dish' => $dishes], 200);
     }
 
     public function deleteDish($id){
         $dishes = RestModel::find($id);
         if(is_null($dishes)){
-            return response()->json(['status code' => 404, 'status text' => 'Dish not found', 'body'=>'message: Dish not found'], 404);
+            return response()->json(['status code' => 404, 'status text' => 'Dish not found', 'message' => 'Dish not found'], 404);
         }
         $dishes->where('id', $id)->delete();
-        return response()->json(['status code' => 201,'status text' => "Successful delete", 'body'=>'message: Dish not found'], 201);
+        return response()->json(['status code' => 201,'status text' => "Successful delete", 'message' => 'Dish not found'], 201);
     }
 }
